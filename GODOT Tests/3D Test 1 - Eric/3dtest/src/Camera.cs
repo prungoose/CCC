41608884c754b95c747966b8479932a00c1e4b4f
prungoose <ereichard677@gmail.com>
2025-06-07 20:53:53 +0900

    sprite test + reorganizing

--- GODOT Tests/3D Test 1 - Eric/3dtest/src/Camera.cs
+++ GODOT Tests/3D Test 1 - Eric/3dtest/src/Camera.cs
@@ -11,28 +11,21 @@ public partial class Camera : Node3D
 
 	public override void _Ready()
 	{
-		if (_target == null) GD.Print("target null");
 	}
 
 	public override void _Process(double delta)
 	{
-		GlobalTransform = new Transform3D(GlobalTransform.Basis, GlobalTransform.Origin.Lerp(_target.GlobalTransform.Origin, (float)delta * _followspeed));
+		GlobalTransform = new Transform3D(GlobalTransform.Basis, GlobalTransform.Origin.Lerp(_target.GlobalTransform.Origin, _followspeed * (float)delta));
 		_SmoothOrbit(delta);
 	
 	}
 
 	void _SmoothOrbit(double delta)
 	{
-		float input = 0;
-		if (Input.IsActionPressed("orbit_left"))
-			input += 1;
-		if (Input.IsActionPressed("orbit_right"))
-			input -= 1;
-
-		if (input != 0)
-		{
-			_targetangle += .015f * input;
-		}
+		if (Input.IsActionJustPressed("orbit_left"))
+			_targetangle += Mathf.DegToRad(45);
+		if (Input.IsActionJustPressed("orbit_right"))
+			_targetangle -= Mathf.DegToRad(45);
 
 		float _angle = Mathf.LerpAngle(this.Rotation.Y, _targetangle, _orbitspeed * (float)delta);
 		this.Rotation = new Godot.Vector3(0, _angle, 0);
--- GODOT Tests/3D Test 1 - Eric/3dtest/src/Controller.cs
+++ GODOT Tests/3D Test 1 - Eric/3dtest/src/Controller.cs
@@ -15,10 +15,13 @@ public partial class Controller : CharacterBody3D
 	[Export] public Node3D _headtarget;
 	[Export] public Node3D _campivot;
 	private Node3D _head;
+	private AnimationPlayer _anim;
+	bool face_left = false;
 
 	public override void _Ready()
 	{
 		_head = GetNode<Node3D>("Head");
+		_anim = GetNode<AnimationPlayer>("WorldModel/Sprite3D/AnimationPlayer");
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -26,6 +29,42 @@ public partial class Controller : CharacterBody3D
 		_Movement((float)delta);
 		_head.LookAt(_headtarget.GlobalPosition, Godot.Vector3.Forward);
 		_HandleCollisions();
+
+	}
+
+	public override void _Process(double delta)
+	{
+		Godot.Vector2 inputdir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+
+		if (inputdir.X != 0)
+		{
+			if (inputdir.X > 0)
+			{
+				_anim.Play("walk_right");
+				face_left = false;
+			}
+			else
+			{
+				_anim.Play("walk_left");
+				face_left = true;
+			}
+		}
+		else if (inputdir.Y != 0)
+		{
+			if (face_left)
+			{
+				_anim.Play("walk_left");
+			}
+			else
+			{
+				_anim.Play("walk_right");
+			}
+		}
+
+		else
+		{
+			_anim.Play("idle");
+		}
 
 	}
 
