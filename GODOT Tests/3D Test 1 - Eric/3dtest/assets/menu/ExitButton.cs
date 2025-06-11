bff842d6019a9165b700597421f107159f72cdf6
SonyaBW <sonyawong@ufl.edu>
2025-06-11 11:41:23 +0900

    added start button and exit button

---
+++ GODOT Tests/3D Test 1 - Eric/3dtest/assets/menu/ExitButton.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class ExitButton : Button
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void _ExitButtonPressed()
+	{
+		GetTree().Quit();
+	}
+}
