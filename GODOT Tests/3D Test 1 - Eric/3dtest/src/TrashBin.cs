f791ab625bdbdecb68399aabb0e041bf7bb8de56
prungoose <ereichard677@gmail.com>
2025-06-15 02:45:36 +0900

    sprite cranberry in stores now

--- GODOT Tests/3D Test 1 - Eric/3dtest/src/TrashBin.cs
+++ GODOT Tests/3D Test 1 - Eric/3dtest/src/TrashBin.cs
@@ -11,7 +11,7 @@ public partial class TrashBin : Area3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		GD.Print(playerEntry);
+		//GD.Print(playerEntry);
 		if (playerEntry && Input.IsActionPressed("jump"))
 		{
 
