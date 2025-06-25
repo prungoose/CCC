58b9a1d2502466219bc0775ae1398bf4a0532852
SonyaBW <sonyawong@ufl.edu>
2025-06-26 01:17:56 +0900

    +adjustable sound effects vol

--- GODOT Tests/3D Test 1 - Eric/3dtest/assets/menu/MainMenuButton.cs
+++ GODOT Tests/3D Test 1 - Eric/3dtest/assets/menu/MainMenuButton.cs
@@ -9,13 +9,12 @@ public partial class MainMenuButton : Button
 	private OptionButton timebutton;
 	public ConfigFile CF = new ConfigFile();
 	[Export] public VBoxContainer parent;
-	
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		//var parent = GetParent<Control>().GetNode<VBoxContainer>("Adjustments");
-		GD.Print(parent.Name);
+		var parent = GetParent<Control>().GetNode<VBoxContainer>("Adjustments");
+		//GD.Print(parent.Name);
 		volslider = parent.GetNode<HSlider>("VolumeSlider");
 		sfxslider = parent.GetNode<HSlider>("SoundEffectsSlider");
 		langbutton = parent.GetNode<OptionButton>("LanguageButton");
@@ -44,8 +43,8 @@ public partial class MainMenuButton : Button
 		CF.Save(OS.GetUserDataDir() + "/" + "PlayerSettings.cfg");
 		GD.Print(OS.GetUserDataDir());
 	}
-	
-		public void Load()
+
+	public void Load()
 	{
 		if (CF.Load(OS.GetUserDataDir() + "/" + "PlayerSettings.cfg") != Error.Ok)
 		{
@@ -59,4 +58,6 @@ public partial class MainMenuButton : Button
 			timebutton.Selected = (int)CF.GetValue("playersettings", "time", timebutton.Selected);
 		}
 	}
+
+	
 }
