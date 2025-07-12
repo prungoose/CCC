02dd5cf77e7ccd02b58000a3b31d2c1060e03780
prungoose <ereichard677@gmail.com>
2025-07-12 21:57:26 +0900

    trashbag sprites, obstacle sprite fix

--- GODOT Tests/3D Test 1 - Eric/3dtest/src/ThrownTrash.cs
+++ GODOT Tests/3D Test 1 - Eric/3dtest/src/ThrownTrash.cs
@@ -5,6 +5,7 @@ public partial class ThrownTrash : RigidBody3D
 {
 	[Export] private PackedScene _trashscene;
 	private CharacterBody3D _player;
+	private Sprite3D _sprite;
 	private int _bounces;
 	private int _id;
 
@@ -12,6 +13,15 @@ public partial class ThrownTrash : RigidBody3D
 	{
 		_player = GetTree().CurrentScene.GetNode<CharacterBody3D>("SubViewportContainer/SubViewport/Player");
 		_id = (int)_player.Call("GetCurrentThrowing");
+		_sprite = GetNode<Sprite3D>("Sprite3D");
+
+		switch (_id)
+		{
+			case 1: _sprite.Frame = 2; break;
+			case 2: _sprite.Frame = 1; break;
+			case 3: _sprite.Frame = 0; break;
+			case 4: _sprite.Frame = 3; break;
+		}
 	}
 
 	public override void _Process(double delta)
